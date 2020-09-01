8438de5ab5c4a2e1263e889b73b1fd797266477c
FengHuan98 <2307768232>
2020-09-01 20:19:15 +0800

    第三次提交

---
+++ demo/src/main/java/com/example/demo/controller/SysUserController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.SysUser;
+import com.example.demo.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+import java.util.List;
+@RestController
+public class SysUserController {
+
+    @Autowired
+    private SysUserService sysUserService;
+
+    @GetMapping("/getUser")
+    public SysUser getUser(Long id){
+
+//        if(id!=null)
+//        {
+//            return sysUserService.getById(id);
+//
+//        }else{
+//
+//            return null;
+//        }
+         return sysUserService.getById(id);
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(SysUser sysUser){
+        System.out.println(sysUser.toString()+"冯欢");
+        sysUser.setCreateTime(new Date());
+        return sysUserService.save(sysUser)?"保存成功":"保存失败";
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser(SysUser sysUser){
+        return sysUserService.updateById(sysUser)?"修改成功":"修改失败";
+    }
+
+    @GetMapping("/list")
+    public List<SysUser> list(){
+        return sysUserService.list();
+    }
+}
--- demo/src/main/java/com/example/demo/entity/SysUser.java
+++ demo/src/main/java/com/example/demo/entity/SysUser.java
@@ -1,6 +1,76 @@
 package com.example.demo.entity;
 
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class SysUser {
     private Long id;
     private String userName;
+    private String passWord;
+    private Date createTime;
+    private Integer userAge;
+    private String remark;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(Integer userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public String toString() {
+        return "SysUser{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", passWord='" + passWord + '\'' +
+                ", createTime=" + createTime +
+                ", userAge=" + userAge +
+                ", remark='" + remark + '\'' +
+                '}';
+    }
 }
